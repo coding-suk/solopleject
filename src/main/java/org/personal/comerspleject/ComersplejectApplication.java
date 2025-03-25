f5c1fa5149205f70d0c3655eceae23d82c078593
coding-suk <hjldd0523@naver.com>
2025-03-25 16:54:45 +0900

    first init

---
+++ src/main/java/org/personal/comerspleject/ComersplejectApplication.java
@@ -0,0 +1,13 @@
+package org.personal.comerspleject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComersplejectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComersplejectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/personal/comerspleject/ComersplejectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.personal.comerspleject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComersplejectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
