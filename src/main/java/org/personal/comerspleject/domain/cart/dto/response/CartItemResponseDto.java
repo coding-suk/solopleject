5cc28964b34672e5c3bd04b1be3c115d6ac5156f
coding-suk <hjldd0523@naver.com>
2025-06-01 20:26:58 +0900

    장바구니 기능

--- src/main/java/org/personal/comerspleject/domain/cart/dto/response/CartItemResponseDto.java
+++ src/main/java/org/personal/comerspleject/domain/cart/dto/response/CartItemResponseDto.java
@@ -1,6 +1,7 @@
 package org.personal.comerspleject.domain.cart.dto.response;
 
 import lombok.Getter;
+import org.personal.comerspleject.domain.cart.entity.CartItem;
 
 @Getter
 public class CartItemResponseDto {
@@ -15,10 +16,20 @@ public class CartItemResponseDto {
 
     private Integer price;
 
-    public CartItemResponseDto(String productName, int quantity, Integer price) {
+    public CartItemResponseDto(Long productId, String productName, int quantity, Integer price) {
+        this.productId = productId;
         this.productName = productName;
         this.quantity = quantity;
         this.price = price;
+    }
+
+    public static CartItemResponseDto from(CartItem item) {
+        return new CartItemResponseDto(
+                item.getProduct().getPId(),
+                item.getProduct().getName(), // Product에 getName()이 있다고 가정
+                item.getProduct().getPrice(), // Product에 getPrice()가 있다고 가정
+                item.getQuantity()
+        );
     }
 
 }
