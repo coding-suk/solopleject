abff63986299d2ef3ce337f7f8a42740a37a8b02
coding-suk <hjldd0523@naver.com>
2025-06-24 15:38:28 +0900

    배송대기 추가

--- src/main/java/org/personal/comerspleject/domain/order/entity/OrderStatus.java
+++ src/main/java/org/personal/comerspleject/domain/order/entity/OrderStatus.java
@@ -2,8 +2,9 @@ package org.personal.comerspleject.domain.order.entity;
 
 public enum OrderStatus {
 
-    Waiting_for_delivery,
-    delivery,
-    delivery_completed;
+    WAITING_FOR_PAYMENT, // 결제 전
+    WAITING_FOR_DELIVERY, // 결제 완료 후 배송대기, 결제 완료, 배송 준비중
+    DELIVERY, // 배송중
+    DELIVERY_COMPLETED; // 배송 완료
 
 }
