b219f886977ed1627c13fd55a6505c444b723ecf
coding-suk <hjldd0523@naver.com>
2025-07-29 13:31:05 +0900

    토큰 로그 추가 + user목록

--- src/main/java/org/personal/comerspleject/config/jwt/JwtAuthenticationToken.java
+++ src/main/java/org/personal/comerspleject/config/jwt/JwtAuthenticationToken.java
@@ -2,13 +2,16 @@ package org.personal.comerspleject.config.jwt;
 
 import org.personal.comerspleject.domain.auth.entity.AuthUser;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.List;
 
 public class JwtAuthenticationToken extends AbstractAuthenticationToken {
 
     private final AuthUser authUser;
 
     public JwtAuthenticationToken(AuthUser authUser) {
-        super(authUser.getAuthorities());
+        super(List.of(new SimpleGrantedAuthority("ROLE_" + authUser.getUserRole().name())));
         this.authUser = authUser;
         setAuthenticated(true);
     }
