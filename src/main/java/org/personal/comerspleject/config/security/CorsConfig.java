cc33c7478da0dbe5128158cb68e32043afac53b7
coding-suk <hjldd0523@naver.com>
2025-08-09 07:12:42 +0900

    주소 수정,

--- src/main/java/org/personal/comerspleject/config/security/CorsConfig.java
+++ src/main/java/org/personal/comerspleject/config/security/CorsConfig.java
@@ -1,10 +1,13 @@
 package org.personal.comerspleject.config.security;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -28,14 +31,22 @@ public class CorsConfig {
 //    }
 
     @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilterRegistrationBean() {
+        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(corsConfigurationSource()));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE); // 필터 체인에서 최우선 적용
+        return bean;
+    }
+
+    @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
         // ✅ 프론트에서 접근하는 모든 Origin을 명시
-        config.setAllowedOrigins(List.of(
+        config.setAllowedOriginPatterns(List.of(
                 "http://localhost:5173",
-                "http://localhost:5174",
-                "http://ecomos-frontend.s3-website-ap-northeast-2.amazonaws.com"
+                "http://127.0.0.1:5173",
+                "http://ecomos-frontend.s3-website.ap-northeast-2.amazonaws.com",
+                "https://ecomos-frontend.s3-website.ap-northeast-2.amazonaws.com"
         ));
 
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
@@ -47,4 +58,9 @@ public class CorsConfig {
         source.registerCorsConfiguration("/**", config);
         return source;
     }
+    /**
+     * CORS(Cross-Origin Resource Sharing)**는
+     * "브라우저가 **다른 출처(origin)**로 API 요청을 보내는 것을
+     * 허용할지 말지 서버에게 물어보는 정책"
+     */
 }
